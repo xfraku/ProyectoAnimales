f584dcf1314539c4b42eceaa4200a5be9958b757
Franco Roncal Ruiz <84628300+xfraku@users.noreply.github.com>
2026-06-05 12:23:06 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
-import pg8000.dbapi
+import pg8000
 import streamlit as st
 
 def get_db_connection():
     try:
         if "DB_HOST" in st.secrets:
-            # Creamos la conexión a AWS
-            conn = pg8000.dbapi.connect(
+            # Conexión a AWS RDS
+            return pg8000.connect(
                 host=st.secrets["DB_HOST"],
                 database=st.secrets["DB_NAME"],
                 user=st.secrets["DB_USER"],
@@ -14,8 +14,8 @@ def get_db_connection():
                 timeout=5
             )
         else:
-            # Creamos la conexión Local
-            conn = pg8000.dbapi.connect(
+            # Conexión Local
+            return pg8000.connect(
                 host="localhost",
                 database="petskin_db",
                 user="postgres",
@@ -23,11 +23,6 @@ def get_db_connection():
                 port=5432,
                 timeout=3
             )
-        
-        # 🔥 EL TRUCO CORREGIDO: Activa el modo diccionario nativo para pg8000
-        conn.run_mapper = pg8000.dbapi.dict_row_mapper
-        return conn
-
     except Exception as e:
         st.error(f"❌ Error de conexión detallado: {e}")
         st.stop()
