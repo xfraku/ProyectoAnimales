8dc8bda74ff7cdd500619ebc3237db73c5fe4ea5
Franco Roncal Ruiz <84628300+xfraku@users.noreply.github.com>
2026-06-05 12:14:26 -0500

    Update database.py

--- database.py
+++ database.py
@@ -3,27 +3,31 @@ import streamlit as st
 
 def get_db_connection():
     try:
-        # Si está en la nube, usa los Secrets de AWS con pg8000
         if "DB_HOST" in st.secrets:
-            return pg8000.connect(
+            # Creamos la conexión a AWS
+            conn = pg8000.connect(
                 host=st.secrets["DB_HOST"],
                 database=st.secrets["DB_NAME"],
                 user=st.secrets["DB_USER"],
                 password=st.secrets["DB_PASSWORD"],
-                port=int(st.secrets["DB_PORT"]),  # pg8000 pide el puerto como número entero
+                port=int(st.secrets["DB_PORT"]),
                 timeout=5
             )
+        else:
+            # Creamos la conexión Local
+            conn = pg8000.connect(
+                host="localhost",
+                database="petskin_db",
+                user="postgres",
+                password="Farjevasquez16*",
+                port=5432,
+                timeout=3
+            )
         
-        # Entorno local (puedes dejarlo con psycopg2 o cambiarlo también, pero para probar la nube usemos pg8000)
-        return pg8000.connect(
-            host="localhost",
-            database="petskin_db",
-            user="postgres",
-            password="Farjevasquez16*",
-            port=5432,
-            timeout=3
-        )
+        # 🔥 TRUCO CLAVE: Configuramos la conexión para que use diccionarios de forma nativa
+        conn.run_mapper = pg8000.types.dict_row_mapper
+        return conn
+
     except Exception as e:
-        # Capturamos cualquier error para que nos avise
         st.error(f"❌ Error de conexión detallado: {e}")
         st.stop()
